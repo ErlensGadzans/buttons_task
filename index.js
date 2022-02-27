1ce3dcb103115e38f672d7fda1a515a9518f4269
ErlensGadzans  <erlens.gadzans@gmail.com>
2022-02-27 18:48:48 +0200

    remove button func. added

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const container = document.querySelector(".container");
 let last_number = 42;
 
-for (let i = 1; i < 43; i++) {
+for (let i = 1; i <= last_number; i++) {
   addButton(i);
 }
 
-document.querySelector(".add").onclick = function () {
+document.querySelector(".add").onclick = function (event) {
   addButton(++last_number);
+};
+
+document.querySelector(".remove").onclick = function (event) {
+  //   console.log(container.children);
+  //   console.log("remove button clicked");
+  container.children[--last_number].remove();
 };
 
 function addButton(event) {

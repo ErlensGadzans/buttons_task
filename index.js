4d691971e92db33d8be178655b371a574f56afaf
ErlensGadzans  <erlens.gadzans@gmail.com>
2022-02-27 17:32:44 +0200

    add button added functionality

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const container = document.querySelector(".container");
+let last_number = 42;
 
 for (let i = 1; i < 43; i++) {
+  addButton(i);
+}
+
+document.querySelector(".add").onclick = function () {
+  addButton(++last_number);
+};
+
+function addButton(event) {
   const button = document.createElement("button"); //creating button
-  button.textContent = i;
+  button.textContent = event;
   container.append(button); //adding button to container
 }
